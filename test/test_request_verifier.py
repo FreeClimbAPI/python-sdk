766184be19054a578019c032cc44724ce21bc411
Ajevan M <amahadaya@vailsys.com>
2023-03-30 10:28:44 -0400

    Make edits to last test in request verifier test file

--- test/test_request_verifier.py
+++ test/test_request_verifier.py
@@ -105,14 +105,17 @@ class TestRequestVerifier(unittest.TestCase):
             self.model.verifyRequestSignature(requestBody, requestHeader, signingSecret, tolerance)
         self.assertEqual(str(exc.exception), "Unverified signature request, If this request was unexpected, it may be from a bad actor. Please proceed with caution. If the request was exepected, please check any typos or issues with the signingSecret")
     
-    def test_verifySignature(self):
+    def test_verifyRequestSignature(self):
         requestBody = "{\"accountId\":\"AC1334ffb694cd8d969f51cddf5f7c9b478546d50c\",\"callId\":\"CAccb0b00506553cda09b51c5477f672a49e0b2213\",\"callStatus\":\"ringing\",\"conferenceId\":null,\"direction\":\"inbound\",\"from\":\"+13121000109\",\"parentCallId\":null,\"queueId\":null,\"requestType\":\"inboundCall\",\"to\":\"+13121000096\"}"
-        signingSecret = "sigsec_ead6d3b6904196c60835d039e91b3341c77a7794"
+        signingSecret = "sigsec_ead6d3b6904196c60835d039e91b3341c77a7793"
         tolerance = 5 * 60
         requestHeader = "t=1679944186,v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8"        
-        with self.assertRaises(Exception) as exc:
+        raised = False
+        try:
             self.model.verifyRequestSignature(requestBody, requestHeader, signingSecret, tolerance)
-        self.assertEqual(str(exc.exception), "Unverified signature request, If this request was unexpected, it may be from a bad actor. Please proceed with caution. If the request was exepected, please check any typos or issues with the signingSecret")
+        except:
+            raised = True
+        self.assertFalse(raised, 'Exception has been raised')
     
     
 
