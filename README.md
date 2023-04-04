a0ed86dd47304724c5bb818eded94140f7b82da4
Ajevan M <amahadaya@vailsys.com>
2023-04-04 12:25:29 -0400

    Edit README doc and request verifier class as per general feedback

--- README.md
+++ README.md
@@ -319,9 +319,11 @@ Class | Method | HTTP request | Description
 
 - To verify the request signature, we will need to use the verify_request_signature method within the Request Verifier class
 
-  RequestVerifier.verify_request_signature()
+  RequestVerifier.verify_request_signature(requestBody, requestHeader, signingSecret, tolerance)
 
   This is a method that you can call directly from the request verifier class, it will throw exceptions depending on whether all parts of the request signature is valid otherwise it will throw a specific error message depending on which request signature part is causing issues
+
+  This method requires a requestBody of type string, a requestHeader of type string, a signingSecret of type string, and a tolerance value of type int
 
   Example code down below
 
--- freeclimb/utils/request_verifier.py
+++ freeclimb/utils/request_verifier.py
@@ -8,19 +8,20 @@ class RequestVerifier:
 
     @staticmethod
     def verify_request_signature(request_body:str, request_header:str, signing_secret:str, tolerance:int=DEFAULT_TOLERANCE):
-        RequestVerifier.check_request_body(request_body)
-        RequestVerifier.check_request_header(request_header)
-        RequestVerifier.check_signing_secret(signing_secret)
-        RequestVerifier.check_tolerance(tolerance)
+        verifier = RequestVerifier()
+        verifier.check_request_body(request_body)
+        verifier.check_request_header(request_header)
+        verifier.check_signing_secret(signing_secret)
+        verifier.check_tolerance(tolerance)
         info = SignatureInformation(request_header)
-        RequestVerifier.verify_tolerance(info, tolerance)
-        RequestVerifier.verify_signature(info, request_body, signing_secret)
+        verifier.verify_tolerance(info, tolerance)
+        verifier.verify_signature(info, request_body, signing_secret)
     
-    def check_request_body(request_body:str):
+    def check_request_body(self, request_body:str):
         if request_body == "" or request_body == None:
             raise Exception("Request Body cannot be empty or null")
 
-    def check_request_header(request_header:str): 
+    def check_request_header(self, request_header:str): 
         if request_header == "" or request_header == None: 
             raise Exception("Error with request header, Request header is empty")
         
@@ -31,21 +32,21 @@ class RequestVerifier:
             raise Exception("Error with request header, signatures are not present")
         
 
-    def check_signing_secret(signing_secret:str):
+    def check_signing_secret(self, signing_secret:str):
         if signing_secret == "" or signing_secret == None:
             raise Exception("Signing secret cannot be empty or null")
         
-    def check_tolerance(tolerance:int):
+    def check_tolerance(self, tolerance:int):
         if tolerance <= 0 or tolerance >= sys.maxsize:
             raise Exception("Tolerance value must be a positive integer")
         
-    def verify_tolerance(info:SignatureInformation, tolerance:int):
+    def verify_tolerance(self, info:SignatureInformation, tolerance:int):
         currentTime = info.get_current_unix_time()
         if not(info.is_request_time_valid(tolerance)):
             raise Exception("Request time exceeded tolerance threshold. Request: " + str(info.request_timestamp)
             + ", CurrentTime: " + str(currentTime) + ", tolerance: " + str(tolerance))
 
-    def verify_signature(info:SignatureInformation, request_body:str, signing_secret:str):
+    def verify_signature(self, info:SignatureInformation, request_body:str, signing_secret:str):
          if not(info.is_signature_safe(request_body, signing_secret)):
             raise Exception("Unverified signature request, If this request was unexpected, it may be from a bad actor. Please proceed with caution. If the request was exepected, please check any typos or issues with the signingSecret")
 
