800565864353a357ac3d9062df32e18a4451301c
Ajevan M <amahadaya@vailsys.com>
2023-05-01 19:33:47 -0400

    Extract timestamp into variable for python sdk

--- test/test_signature_information.py
+++ test/test_signature_information.py
@@ -3,11 +3,13 @@ import unittest
 
 from freeclimb.utils.signature_information import SignatureInformation
 
+time_stamp = 1679944186
+
 class TestSignatureInformation(unittest.TestCase):
     """SignatureInformation unit test stubs"""
-
+    global time_stamp
     def setUp(self):
-        request_header = "t=1679944186,v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8"
+        request_header = "t=" + str(time_stamp) + ",v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8"
         self.signature_information = SignatureInformation(request_header)
     
     def tearDown(self):
@@ -18,7 +20,7 @@ class TestSignatureInformation(unittest.TestCase):
         self.assertEqual(self.signature_information.is_request_time_valid(tolerance), True)
     
     def test_is_request_time_valid_false(self):
-        tolerance = sys.maxsize - 1679944186
+        tolerance = sys.maxsize - time_stamp
         self.assertEqual(self.signature_information.is_request_time_valid(tolerance), False)
     
     def test_is_signature_safe_true(self):
