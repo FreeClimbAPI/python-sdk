86438fe7bbae5a62bc74b7da2468d39292b05f16
Ajevan M <amahadaya@vailsys.com>
2023-09-07 09:33:26 -0400

    Ensure TranscribeUtterance is a PerclCommand

--- freeclimb/model/transcribe_utterance.py
+++ freeclimb/model/transcribe_utterance.py
@@ -350,4 +350,24 @@ class TranscribeUtterance(ModelNormal):
         if mapping == None:
             raise ApiAttributeError("{0} has no mapping '{1}'".format(TranscribeUtterance.__class__.name, 'command'))
         return mapping
+    
+    @cached_property
+    def _composed_schemas():
+        # we need this here to make our import statements work
+        # we must store _composed_schemas in here so the code is only run
+        # when we invoke this method. If we kept this at the class
+        # level we would get an error because the class level
+        # code would be run when this module is imported, and these composed
+        # classes don't exist yet because their module has not finished
+        # loading
+        lazy_import()
+        return {
+          'anyOf': [
+          ],
+          'allOf': [
+              PerclCommand,
+          ],
+          'oneOf': [
+          ],
+        }
 
